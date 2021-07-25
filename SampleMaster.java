eae2f10f650cc17f2c64e9f5cb26db1ccb7e019e
kicha3101 <kicha3101@gmail.com>
2021-07-25 13:14:02 +0530

    folder

---
+++ SampleMaster.java
@@ -0,0 +1,17 @@
+package org.master;
+
+public class SampleMaster {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Master 1");
+	
+		System.out.println("Master 2");
+	
+	
+	}
+
+	
+	
+}
